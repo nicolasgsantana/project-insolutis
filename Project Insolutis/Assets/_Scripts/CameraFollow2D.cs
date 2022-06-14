e14dfd8f7105a921fde9e82eaf0e64c279cf8384
Nicorovisk <nicolas07san@gmail.com>
2022-06-13 22:58:39 -0300

    camera update + bugs discovered

--- Project Insolutis/Assets/_Scripts/CameraFollow2D.cs
+++ Project Insolutis/Assets/_Scripts/CameraFollow2D.cs
@@ -6,12 +6,19 @@ public class CameraFollow2D : MonoBehaviour
 {
     [SerializeField] private GameObject player;
 
-    [SerializeField] private float timeOffset;
+    [SerializeField] private float timeOffset = 5f;
 
     [SerializeField] private Vector2 posOffset;
 
     private Vector3 startPos;
-    [SerializeField]private Vector3 endPos;
+    private Vector3 endPos;
+
+    private Vector3 playerCenter;
+
+    [SerializeField] private float leftLimit;
+    [SerializeField] private float rightLimit;
+    [SerializeField] private float bottomLimit;
+    [SerializeField] private float topLimit;
 
     private void Update()
     {
@@ -20,18 +27,47 @@ public class CameraFollow2D : MonoBehaviour
         //Player current position
         endPos = player.transform.position;
 
-        posOffset.x = PlayerMovement.mousePos.x/6;
-        posOffset.y = PlayerMovement.mousePos.y/6;
+        playerCenter = player.GetComponent<Renderer>().bounds.center;
+
+        posOffset.x = PlayerMovement.mousePos.x/2;
+        posOffset.y = PlayerMovement.mousePos.y/2;
 
         endPos.x += posOffset.x;
         endPos.y += posOffset.y;
         endPos.z = -10;
 
         
+
+
+
     }
 
     private void FixedUpdate()
     {
         transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, playerCenter.x - leftLimit, playerCenter.x + rightLimit),
+            Mathf.Clamp(transform.position.y, playerCenter.y - bottomLimit, playerCenter.y + topLimit),
+            transform.position.z
+            ); ;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(new Vector2(playerCenter.x - leftLimit, playerCenter.y + topLimit), 
+            new Vector2(playerCenter.x + rightLimit, playerCenter.y + topLimit));
+
+        Gizmos.DrawLine(new Vector2(playerCenter.x + rightLimit, playerCenter.y + topLimit), 
+            new Vector2(playerCenter.x + rightLimit, playerCenter.y - bottomLimit));
+
+        Gizmos.DrawLine(new Vector2(playerCenter.x + rightLimit, playerCenter.y - bottomLimit),
+            new Vector2(playerCenter.x - leftLimit, playerCenter.y - bottomLimit));
+
+        Gizmos.DrawLine(new Vector2(playerCenter.x - leftLimit, playerCenter.y - bottomLimit),
+            new Vector2(playerCenter.x - leftLimit, playerCenter.y + topLimit));
+
     }
 }
